1ac60f54e41ebaef036ce1cf72d6f04c426395be
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-26 21:26:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
 
   def self.artist_count
     @@artists.each do |artist|
-      @@artist_count[artist] = @@artist.count(artist)
+      @@artist_count[artist] = @@artists.count(artist)
     end
       @@artist_count
   end
