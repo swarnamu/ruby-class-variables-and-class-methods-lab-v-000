7c301ae7ea4893ec4f8bd172a516d935690e3674
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-26 21:03:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,13 @@ class Song
 
   attr_accessor :name, :artists, :genre
   attr_reader :artist
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  @@artist_count = {}
+
   def initialize(name, artist, genre)
 
   end
